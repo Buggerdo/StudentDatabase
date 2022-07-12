7858d9cc4b6486a64cae8f9913a45dd2afbb6d7b
Troy <wilson.troy.anthony@gmail.com>
2022-07-11 21:59:29 -0400

    Added some code

---
+++ StudentDatabase/GetInput.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDatabase
+{
+    internal class GetInput
+    {
+        public static uint GetInt(int range)
+        {
+            bool isGood = false;
+            uint output = 0;
+
+            do
+            {
+                Console.Clear();
+                Console.Write($"Welcome! Which student would you like to learn more about? Enter a number 1-{range}: ");
+                string input = Console.ReadLine().ToLower().Trim();
+                try
+                {
+                    output = uint.Parse(input);
+                    if(output > 0 && output <= range)
+                    {
+                        isGood = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("That index is not listed");
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine("Sorry that is not a number.");
+                    isGood = false;
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Sorry that number is out of range.");
+                    isGood = false;
+                }
+                if(!isGood)
+                {
+                    Console.WriteLine("Press any key to continue.");
+                    Console.ReadKey();
+                }
+            } while(!isGood);
+
+            return output;
+        }
+    }
+}
--- StudentDatabase/StudentDatabase.cs
+++ StudentDatabase/StudentDatabase.cs
@@ -10,10 +10,39 @@ namespace StudentDatabase
     {
         public StudentDatabase()
         {
+            bool isCategory;
+
             string[] name = { "Troy Wilson" , "Barbara Baker", "Elyas Goddard" , "Marwan Lozano" , "Anayah Church" , "Kobi Preece" , "Tamara Chase" , "Dwayne Wilder" , "Blanche Hess" };
             string[] homeTown = { "Coral" , "Grand Rapids" , "Detroit", "Lansing", "Ann Arbor", "Flint", "Kalamazoo", "Saginaw", "Bay City" };
             string[] favoritFood = { "Pizza" , "Ice Cream" , "Steak", "Waffles", "Beef", "Bacon", "Chicken", "Corned beef", "Pancakes" };
+           
+            string[] home =  { "home town", "home", "town" , "h" , "t" };
+            string[] food = { "favorit food", "favorit", "food" , "f"};
 
+            uint intInput = GetInput.GetInt(name.Length);
+            
+            do
+            {
+                Console.Write($"Student {intInput} is {name[intInput - 1]}. What would you like to know? Enter \"hometown\" or \"favorite food\" : ");
+                string strInput = Console.ReadLine().ToLower().Trim();
+
+                if(home.Contains(strInput))
+                {
+                    Console.WriteLine($"{name[intInput - 1]} is from {homeTown[intInput - 1]}");
+                    isCategory = true;
+                }
+                else if(food.Contains(strInput))
+                {
+                    Console.WriteLine($"{name[intInput - 1]} favorit food is {favoritFood[intInput - 1]}");
+                    isCategory = true;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Sorry that is not a valid input");
+                    isCategory = false;
+                } 
+            } while(!isCategory);
         }
     }
 }
